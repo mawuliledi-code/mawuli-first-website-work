94b0153b305ba46a6dd91a5045504c24d9d69f64
mawuliledi-code <mawuli.ledi@code.berlin>
2026-02-22 11:27:31 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+
+
+.raleway-<uniquifier> {
+  font-family: "Raleway", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: <weight>;
+  font-style: normal;
+}
+
+
+
+
+https://www.ghacem.com/en
---
+++ main.js
@@ -0,0 +1,16 @@
+// Store a reference to the <h1> in a variable
+const myHeading = document.querySelector("h1");
+// Update the text content of the <h1>
+myHeading.textContent = "Joyiah Ventures";
+
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+
+  if (mySrc === "images/ghacem.png") {
+    myImage.setAttribute("src", "images/ghacem-cement.png");
+  } else {
+    myImage.setAttribute("src", "images/ghacem.png");
+  }
+});
